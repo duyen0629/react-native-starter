e8a6edbaf9dceebb907b11cce5bf7d0d4ad4705d
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-06 14:12:37 +1000

    Update Modal Styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -40,19 +40,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
   },
   buttonContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    marginTop: 16,
   },
   button: {
     width: "30%",
