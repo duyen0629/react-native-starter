b15115c0d1cc1f4151fc30c7b3e3c81f36cedb83
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-06 13:19:02 +1000

    Using Prop

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-function GoalItem({ text }) {
+function GoalItem(props) {
   return (
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{text}</Text>
+      <Text style={styles.goalText}>{props.text}</Text>
     </View>
   );
 }
