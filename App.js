249ca34ee44c1fbfcc81b6d3f15474efa6759037
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-12 08:55:08 +1000

    Add Category Screen

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-import { StyleSheet, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer} />
-    </>
-  );
+  return <CategoriesScreen />;
 }
 
 const styles = StyleSheet.create({
