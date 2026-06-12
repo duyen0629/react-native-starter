d8c21701534e8d401b2c4a64e4ec600a1a204c2c
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-13 08:14:51 +1000

    Load meal list in Favorite Screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from "../data/dummy-data";
+
 function FavoritesScreen() {
-  return (
-    <View>
-      <Text>Favorites Screen</Text>
-    </View>
-  );
+  const favoritesContext = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) => favoritesContext.ids.includes(meal.id));
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
