a0d9ebdf6a85ad6b4d6b69c607e1133c5703de5a
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-12 10:40:14 +1000

    Display Meal with meal item

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) =>
+    mealItem.categoryIds.includes(catId),
+  );
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
