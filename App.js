55e3013481c5e64cd118fa45dd5f154e96ba91a7
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-12 10:02:19 +1000

    Add navigation lib and library

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StyleSheet, View, StatusBar } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
