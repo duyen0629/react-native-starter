1c439c7aaf29b512886fb8772eea537c3b3c552e
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-13 08:17:18 +1000

    Add style for Meal List

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,3 +1,4 @@
+import { View, Text, StyleSheet } from "react-native";
 import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import MealsList from "../components/MealsList/MealsList";
@@ -6,7 +7,29 @@ import { MEALS } from "../data/dummy-data";
 function FavoritesScreen() {
   const favoritesContext = useContext(FavoritesContext);
   const favoriteMeals = MEALS.filter((meal) => favoritesContext.ids.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No favorite meals found. Start adding some!</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
